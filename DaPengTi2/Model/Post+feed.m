73d6a4b3416340511e4dda10408dd49a25c8e14f
konglingliang <konglingliang@corp.the9.com>
2012-07-02 15:02:19 +0800

    fix

---
+++ DaPengTi2/Model/Post+feed.m
@@ -0,0 +1,35 @@
+//
+//  Post+feed.m
+//  DaPengTi2
+//
+//  Created by  on 12-6-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "Post+feed.h"
+
+@implementation Post (feed)
++ (Post *)postsFromFeed:(NSDictionary *)feed inManagedObjectContext:(NSManagedObjectContext *)context{
+    Post *post = nil;
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Post"];
+    request.predicate = [NSPredicate predicateWithFormat:@"postID = %@", [feed valueForKey:@"post_id"]];
+    
+    NSError *error = nil;
+    NSArray *matches = [context executeFetchRequest:request error:&error];	
+    
+    if (!matches || ([matches count] > 1)) {
+        // handle error
+    } else if (![matches count]) {
+        post = [NSEntityDescription insertNewObjectForEntityForName:@"Post"
+                                              inManagedObjectContext:context];
+        post.title = [feed valueForKey:@"title"];
+        post.postID = [feed valueForKey:@"post_id"];
+        post.content = [feed valueForKey:@"content"];
+        post.layout  = [feed valueForKey:@"layout"];
+    } else {
+        post = [matches lastObject];
+    }
+    
+    return post;
+}
+@end
